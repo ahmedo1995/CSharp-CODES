da7b61206f53c6388dc08dba8afc1bf54732acae
ahmedo1995 <110618509+ahmedo1995@users.noreply.github.com>
2022-08-08 15:22:24 +0100

    Add files via upload

---
+++ Bank/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Customer
+    {
+        public int Bvn { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public int NIN { get; set; }
+        public string Branch { get; set; }
+        public int age { get; set; }
+
+        public string Openaccount()
+        {
+            if (age >= 18)
+            {
+                return "You can now open your account";
+            }
+            else
+            {
+                return "You cannot open an account";
+            }
+        }
+
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Customer customer = new Customer();
+            Console.WriteLine("please enter your name");
+            string name = Console.ReadLine();
+            customer.name = name;
+            Console.WriteLine("please enter your Bvn");
+            int bvn = Convert.ToInt32(Console.ReadLine());
+            customer.Bvn = bvn;
+            Console.WriteLine("Please enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            customer.age = age;
+
+
+            Console.WriteLine(customer.Openaccount());
+
+        }
+    }
+}
